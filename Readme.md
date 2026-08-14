3303e7c15e3ce8338e9f3a6626d04b2689da87e6
Roland Kenneth P <rolndknnth@gmail.com>
2026-08-14 23:06:22 +0530

    day one complete

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# SQL 20 Days
+
+A 20-day journey to revise and strengthen SQL concepts, from fundamentals to advanced SQL and PostgreSQL.
+
+## Progress
+
+- [x] Day 01 — SQL Fundamentals
+- [ ] Day 02 — Filtering & Data Manipulation
+- [ ] Day 03 — SQL Functions
+- [ ] Day 04 — GROUP BY & HAVING
+- [ ] Day 05 — JOINs
+- [ ] Day 06 — Subqueries
+- [ ] Day 07 — Set Operations
+- [ ] Day 08 — CTEs
+- [ ] Day 09 — Window Functions I
+- [ ] Day 10 — Window Functions II
+- [ ] Day 11 — Advanced PostgreSQL
+- [ ] Day 12 — Database Design & Normalization
+- [ ] Day 13 — Transactions & Concurrency
+- [ ] Day 14 — Indexes
+- [ ] Day 15 — Query Performance
+- [ ] Day 16 — Views, Functions & Triggers
+- [ ] Day 17 — Advanced SQL Problems
+- [ ] Day 18 — Real-World SQL
+- [ ] Day 19 — SQL Project
+- [ ] Day 20 — Interview Revision
---
+++ d1/fundamentals.sql
@@ -0,0 +1,65 @@
+--the things revised
+
+SELECT * FROM users;
+SELECT name,email FROM users;
+SELECT 
+    name As user_name,
+    age AS user_age
+FROM users;
+
+SELECT * FROM users WHERE age > 21;
+
+SELECT * FROM users WHERE age = 25;
+
+SELECT * FROM users WHERE age <= 21;
+
+SELECT * FROM users WHERE age <> 25;
+
+SELECT * FROM users where age > 21 AND name = 'Bob';
+
+SELECT * FROM users where age = 21 OR age = 25;
+
+SELECT * FROM users WHERE name <> 'Alice';
+
+SELECT *
+FROM users
+WHERE name 
+IN ('Alice', 'Bob', 'Charlie');
+
+SELECT * FROM users WHERE age BETWEEN 21 AND 25;
+
+SELECT * FROM users WHERE name LIKE 'A%';
+
+SELECT * FROM users WHERE name LIKE '%e';
+SELECT * FROM users WHERE email LIKE '%gmail%';
+
+SELECT * FROM users WHERE name ILIKE 'a%';
+
+INSERT INTO users (name, email) VALUES 
+    ('David', 'david@gmail.com'),
+    ('Deepa', 'deepa@gmail.com');
+
+SELECT * FROM users WHERE age IS NULL;
+
+SELECT * FROM users WHERE age IS NOT NULL;
+
+SELECT * FROM users ORDER BY age ASC;
+SELECT * FROM users ORDER BY age DESC;
+
+UPDATE users SET age = 23 WHERE name = 'Alice';
+
+SELECT * FROM users LIMIT 2;
+
+SELECT DISTINCT age FROM users;
+
+DELETE FROM users WHERE name = 'David';
+
+
+--answer to the challenge
+
+SELECT * 
+FROM users
+WHERE age BETWEEN 21 AND 25 AND
+    email LIKE '%gmail%'
+ORDER BY age ASC
+LIMIT 3;
